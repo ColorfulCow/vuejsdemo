0ef5ac4a76d1c9521af162487d6577ca08658401
ColorfulCow <2232251493@qq.com>
2016-02-25 13:59:28 +0800

    create project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <v-hello></v-hello>
+    <input v-model="parentMsg">
+    <v-header v-bind:my-message="parentMsg"></v-header>
+  </div>
+</template>
+
+<script>
+import VHello from './components/Hello'
+import VHeader from './components/Header'
+export default {
+  components: {
+    VHello,
+    VHeader
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <span>{{ myMessage }}</span>
+    <input v-model="query">
+    <ul>
+      <li v-for="item in list | filterBy query" transition="staggered">
+        {{ item.msg }}
+      </li>
+    </ul> 
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      query: '',
+      list: [
+        { msg: 'Bruce Lee' },
+        { msg: 'Jackie Chan' },
+        { msg: 'Chuck Norris' },
+        { msg: 'Jet Li' },
+        { msg: 'Kung Fury' }
+      ]
+    }
+  },
+  props: ['myMessage']
+}
+</script>
+
+<style>
+  ul {
+    padding-left: 0;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  .staggered-transition {
+      transition: all .5s ease;
+      overflow: hidden;
+      margin: 0;
+      height: 20px;
+  }
+  .staggered-enter, .staggered-leave {
+      opacity: 0;
+      height: 0;
+  }
+</style>
